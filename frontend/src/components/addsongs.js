b7f8819951668bd8f9f6b82b496b7390e004b87a
suraj <surajtendulkar98@gmail.com>
2022-09-01 21:10:19 +0530

    added add artist & multiselect checkbox funtionality

--- frontend/src/components/addsongs.js
+++ frontend/src/components/addsongs.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
+import {Modal } from "reactstrap";
 import { AiOutlineSearch } from "react-icons/ai";
 import Filebase64 from "react-file-base64";
+import Multiselect from "multiselect-react-dropdown"
 import { GoFileMedia } from "react-icons/go";
 import "./addsong.css"
 
 function AddSongs(){
+
+  const [modalIsopen, setModalIsopen]=useState(false);
+      const [data,setdata] =useState({
+        key:"",
+        cat:"",
+        bio:""
+      });
+      const [options,setoptions]=useState([])
+
+      // let options=[]
+      const handleform=(e)=>{
+        e.preventDefault();
+        console.log(data)
+        // options.push({cat:1,key:data.artist_name})
+      }
+
+      const AddItmes=(e)=>{
+        setoptions([...options,data])
+      }
+
+    
     return (
         <div>
            <div className="home">
@@ -25,6 +48,7 @@ function AddSongs(){
             <div className="date">
                 <label for="name">Date of Release</label>
                   <input type="text" className="songdate" id="date"></input>
+            </div>     
             <div className="image-upload">
                 <div className="label-image">
                    <label for="image">Artwork</label>
@@ -37,11 +61,67 @@ function AddSongs(){
                         multiple={false}
                         title="Add File"
                     />
-                    </div>
-                    
-            </div>
+                </div>
             </div>
             
+            <div className="artistname">
+              <p className="art">Artist</p>
+            </div>
+
+            <div className="multiselect">
+            <Multiselect
+                displayValue="key"
+                onKeyPressFn={function noRefCheck(){}}
+                onRemove={function noRefCheck(){}}
+                onSearch={function noRefCheck(){}}
+                onSelect={function noRefCheck(){}}
+                options={options}
+                showCheckbox
+            />
+            </div>
+
+            <div className="artist-select">  
+          <button className="add-artist" onClick={()=>setModalIsopen(true)}>+ Add Artist</button>  
+          <Modal isOpen={modalIsopen} portalclassname="modal">
+            <form onSubmit={handleform}>
+              <div className="artist-form">
+                <div className="box1">
+                   <p className="title">Add Artist</p>
+                     <button className="close" onClick={()=>setModalIsopen(false)}><b>X</b></button>
+                </div>
+                <hr></hr>
+                <div className="box2">
+                  <div className="cont1">
+                    <label for="artist-name">Artist Name</label>
+                  </div>
+                  <div className="cont2">
+                    <input type="text" id="artist-name"  required={true} onChange={e=>setdata({...data,key: e.target.value})} ></input>
+                  </div>
+                </div>
+                <div className="box2">
+                  <div className="cont1">
+                    <label for="artist-date">Date of Birth</label>
+                  </div>
+                  <div className="cont2">
+                    <input type="date" id="artist-date" onChange={e=>setdata({...data,cat: e.target.value})}></input>
+                  </div>
+                </div>
+                <div className="box2">
+                  <div className="cont1">
+                    <label for="artist-bio">Bio</label>
+                  </div>
+                  <div className="cont2">
+                    <input type="text" id="artist-bio"onChange={e=>setdata({...data,bio: e.target.value})}></input>
+                  </div>
+                </div>
+                <div className="button">
+                  <button className="cancel" onClick={()=>setModalIsopen(false)}>Cancel</button>
+                  <button className="done" type="submit" onClick={AddItmes}>Done</button>
+                </div>
+            </div>
+            </form>
+          </Modal>
+          </div>
 
           </div>
         </div>
