7b96a482a68cee76e76c8401d21ca3a1193a803d
suraj <surajtendulkar98@gmail.com>
2022-09-02 02:24:30 +0530

    create whole song & artist addup form and functionality

--- frontend/src/components/addsongs.js
+++ frontend/src/components/addsongs.js
@@ -4,31 +4,43 @@ import { AiOutlineSearch } from "react-icons/ai";
 import Filebase64 from "react-file-base64";
 import Multiselect from "multiselect-react-dropdown"
 import { GoFileMedia } from "react-icons/go";
+import {Link} from "react-router-dom"
 import "./addsong.css"
 
 function AddSongs(){
 
   const [modalIsopen, setModalIsopen]=useState(false);
-      const [data,setdata] =useState({
-        key:"",
-        cat:"",
-        bio:""
-      });
-      const [options,setoptions]=useState([])
+  const [data,setdata] =useState({
+                                  key:"",
+                                  cat:"",
+                                  bio:""
+                                });
+  const [options,setoptions]=useState([]);
+  const [alldata,setalldata]=useState({
+                                      song_name:"",
+                                      dor:"",
+                                      image:"",
+                                      artist_name:""
+                                     })
+  const [name,setName]=useState(true);
 
       // let options=[]
-      const handleform=(e)=>{
+  const handleform=(e)=>{
         e.preventDefault();
-        console.log(data)
+        // console.log(data)
         // options.push({cat:1,key:data.artist_name})
-      }
+  }
 
-      const AddItmes=(e)=>{
+  const AddItmes=(e)=>{
         setoptions([...options,data])
-      }
+  }
 
+  const handlealldata=(e)=>{
+        e.preventDefault();
+        console.log(alldata)
+  }
     
-    return (
+  return (
         <div>
            <div className="home">
             <div className="header">
@@ -39,15 +51,17 @@ function AddSongs(){
               <input type="text" className="searchtext" placeholder="Search"></input>
             </div>
           </div>
+          <form className="form-data" onSubmit={handlealldata}>
           <div className="song-form">
             <h1>Adding a New Song</h1>
+            <form className="data-from"></form>
             <div className="name">
                 <label for="name">Song Name</label>
-                  <input type="text" className="songname" id="name"></input>
+                  <input type="text" className="songname" id="name" onChange={e=>setalldata({...alldata,song_name: e.target.value})}></input>
             </div>
             <div className="date">
                 <label for="name">Date of Release</label>
-                  <input type="text" className="songdate" id="date"></input>
+                  <input type="text" className="songdate" id="date" onChange={e=>setalldata({...alldata,dor: e.target.value})}></input>
             </div>     
             <div className="image-upload">
                 <div className="label-image">
@@ -59,6 +73,7 @@ function AddSongs(){
                         id="fileimage"
                         type="file"
                         multiple={false}
+                        onDone={({base64}) => {setalldata({ ...alldata,image: base64 });setName(!name)}}
                         title="Add File"
                     />
                 </div>
@@ -72,9 +87,16 @@ function AddSongs(){
             <Multiselect
                 displayValue="key"
                 onKeyPressFn={function noRefCheck(){}}
-                onRemove={function noRefCheck(){}}
-                onSearch={function noRefCheck(){}}
-                onSelect={function noRefCheck(){}}
+                onRemove={(e)=>console.log(e)}
+                onSearch={(e)=>console.log(e)}
+                onSelect={(e)=>{
+                  let arr=[]
+                  for(let ele of e){
+                    arr.push(ele.key)
+                  }
+                  let ans=(String(arr));
+                  setalldata({...alldata,artist_name: ans})
+                }}
                 options={options}
                 showCheckbox
             />
@@ -122,8 +144,10 @@ function AddSongs(){
             </form>
           </Modal>
           </div>
-
           </div>
+          <Link to='/'><button className="cancelhome">Cancel</button></Link>
+          <button type="submit" className="formsubmit">Save</button>
+          </form>
         </div>
     )
 }
