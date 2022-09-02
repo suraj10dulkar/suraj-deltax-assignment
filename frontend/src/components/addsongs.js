760a4713b89768cb1bfa825ebd1657bab7f01d4b
suraj <surajtendulkar98@gmail.com>
2022-09-02 19:17:15 +0530

    added top 10 song functionality

--- frontend/src/components/addsongs.js
+++ frontend/src/components/addsongs.js
@@ -6,9 +6,11 @@ import Multiselect from "multiselect-react-dropdown"
 import { GoFileMedia } from "react-icons/go";
 import {Link} from "react-router-dom"
 import "./addsong.css"
+import {useNavigate} from "react-router-dom"
+import axios from "axios"
 
 function AddSongs(){
-
+  const Navigate = useNavigate();
   const [modalIsopen, setModalIsopen]=useState(false);
   const [data,setdata] =useState({
                                   key:"",
@@ -24,131 +26,144 @@ function AddSongs(){
                                      })
   const [name,setName]=useState(true);
 
-      // let options=[]
-  const handleform=(e)=>{
-        e.preventDefault();
-        // console.log(data)
-        // options.push({cat:1,key:data.artist_name})
-  }
+  // const handleform=(e)=>{
+  //       e.preventDefault();
+  //       // console.log(data)
+  //       // options.push({cat:1,key:data.artist_name})
+  // }
 
   const AddItmes=(e)=>{
         setoptions([...options,data])
+        alert("Artist added in dropdown sucessfully")
   }
 
   const handlealldata=(e)=>{
         e.preventDefault();
-        console.log(alldata)
+        // console.log(alldata)
+        axios({
+          url:"http://localhost:3005/add",
+          method: "POST",
+          data: alldata,
+          headers:{
+          }
+        }).then((post)=>{
+             Navigate("/")
+        }).catch((err)=>{
+             console.log(err)
+        })
+          console.log(alldata)
+          setalldata({song_name:"",dor:"",image: "",artist_name:""})
   }
     
   return (
         <div>
-           <div className="home">
-            <div className="header">
-            </div>
+          <div className="home">
+            <div className="header"></div>
             <div className="main"><b>Home</b></div>
             <div className="search">
               <AiOutlineSearch className="iconsearch"/>
               <input type="text" className="searchtext" placeholder="Search"></input>
             </div>
           </div>
-          <form className="form-data" onSubmit={handlealldata}>
-          <div className="song-form">
-            <h1>Adding a New Song</h1>
-            <form className="data-from"></form>
-            <div className="name">
-                <label for="name">Song Name</label>
-                  <input type="text" className="songname" id="name" onChange={e=>setalldata({...alldata,song_name: e.target.value})}></input>
-            </div>
-            <div className="date">
-                <label for="name">Date of Release</label>
-                  <input type="text" className="songdate" id="date" onChange={e=>setalldata({...alldata,dor: e.target.value})}></input>
-            </div>     
-            <div className="image-upload">
-                <div className="label-image">
-                   <label for="image">Artwork</label>
+          <form className="form-data" onSubmit={handlealldata} method="POST">
+             <div className="song-form">
+                 <h1>Adding a New Song</h1>
+
+                 <div className="name">
+                    <label for="name">Song Name</label>
+                    <input type="text" className="songname" id="name" onChange={e=>setalldata({...alldata,song_name: e.target.value})}></input>
+                 </div>
+
+                 <div className="date">
+                    <label for="name">Date of Release</label>
+                    <input type="text" className="songdate" id="date" onChange={e=>setalldata({...alldata,dor: e.target.value})}></input>
+                 </div>
+
+                 <div className="image-upload">
+                     <div className="label-image">
+                         <label for="image">Artwork</label>
+                     </div>
+                     <div  className="input-file">
+                        <GoFileMedia className="imageicon"/>
+                        <Filebase64
+                          id="fileimage"
+                          type="file"
+                          multiple={false}
+                          onDone={({base64}) => {setalldata({ ...alldata,image: base64 });setName(!name)}}
+                          title="Add File"/>
+                     </div>
                 </div>
-                    <div  className="input-file">
-                    <GoFileMedia className="imageicon"/>
-                     <Filebase64
-                        id="fileimage"
-                        type="file"
-                        multiple={false}
-                        onDone={({base64}) => {setalldata({ ...alldata,image: base64 });setName(!name)}}
-                        title="Add File"
-                    />
-                </div>
-            </div>
             
-            <div className="artistname">
-              <p className="art">Artist</p>
-            </div>
+                <div className="artistname">
+                    <p className="art">Artist</p>
+                </div>
 
-            <div className="multiselect">
-            <Multiselect
-                displayValue="key"
-                onKeyPressFn={function noRefCheck(){}}
-                onRemove={(e)=>console.log(e)}
-                onSearch={(e)=>console.log(e)}
-                onSelect={(e)=>{
-                  let arr=[]
-                  for(let ele of e){
-                    arr.push(ele.key)
-                  }
-                  let ans=(String(arr));
-                  setalldata({...alldata,artist_name: ans})
-                }}
-                options={options}
-                showCheckbox
-            />
-            </div>
+                <div className="multiselect">
+                    <Multiselect
+                       displayValue="key"
+                       onKeyPressFn={function noRefCheck(){}}
+                       onRemove={(e)=>console.log(e)}
+                       onSearch={(e)=>console.log(e)}
+                       onSelect={(e)=>{
+                       let arr=[]
+                       for(let ele of e){
+                       arr.push(ele.key)}
+                       let ans=(String(arr));
+                       setalldata({...alldata,artist_name: ans})}}
+                       options={options}
+                       showCheckbox/>
+                 </div>
 
-            <div className="artist-select">  
-          <button className="add-artist" onClick={()=>setModalIsopen(true)}>+ Add Artist</button>  
-          <Modal isOpen={modalIsopen} portalclassname="modal">
-            <form onSubmit={handleform}>
-              <div className="artist-form">
-                <div className="box1">
-                   <p className="title">Add Artist</p>
-                     <button className="close" onClick={()=>setModalIsopen(false)}><b>X</b></button>
-                </div>
-                <hr></hr>
-                <div className="box2">
-                  <div className="cont1">
-                    <label for="artist-name">Artist Name</label>
-                  </div>
-                  <div className="cont2">
-                    <input type="text" id="artist-name"  required={true} onChange={e=>setdata({...data,key: e.target.value})} ></input>
-                  </div>
-                </div>
-                <div className="box2">
-                  <div className="cont1">
-                    <label for="artist-date">Date of Birth</label>
-                  </div>
-                  <div className="cont2">
-                    <input type="date" id="artist-date" onChange={e=>setdata({...data,cat: e.target.value})}></input>
-                  </div>
-                </div>
-                <div className="box2">
-                  <div className="cont1">
-                    <label for="artist-bio">Bio</label>
-                  </div>
-                  <div className="cont2">
-                    <input type="text" id="artist-bio"onChange={e=>setdata({...data,bio: e.target.value})}></input>
-                  </div>
-                </div>
-                <div className="button">
-                  <button className="cancel" onClick={()=>setModalIsopen(false)}>Cancel</button>
-                  <button className="done" type="submit" onClick={AddItmes}>Done</button>
-                </div>
-            </div>
+                 <div className="artist-select">  
+                      <div className="add-artist" onClick={()=>setModalIsopen(true)}>+ Add Artist</div>  
+                      <Modal isOpen={modalIsopen} portalclassname="modal">
+                        <div className="artist-form">
+
+                          <div className="box1">
+                            <p className="title">Add Artist</p>
+                            <button className="close" onClick={()=>setModalIsopen(false)}><b>X</b></button>
+                          </div>
+                        <hr></hr>
+
+                        <div className="box2">
+                          <div className="cont1">
+                            <label for="artist-name">Artist Name</label>
+                          </div>
+                        <div className="cont2">
+                            <input type="text" id="artist-name"  required={true} onChange={e=>setdata({...data,key: e.target.value})} ></input>
+                        </div>
+                        </div>
+
+                        <div className="box2">
+                          <div className="cont1">
+                            <label for="artist-date">Date of Birth</label>
+                          </div>
+                          <div className="cont2">
+                            <input type="date" id="artist-date" onChange={e=>setdata({...data,cat: e.target.value})}></input>
+                          </div>
+                        </div>
+
+                        <div className="box2">
+                          <div className="cont1">
+                            <label for="artist-bio">Bio</label>
+                          </div>
+                          <div className="cont2">
+                            <input type="text" id="artist-bio"onChange={e=>setdata({...data,bio: e.target.value})}></input>
+                          </div>
+                        </div>
+
+                         <div className="button">
+                          <button className="cancel" onClick={()=>setModalIsopen(false)}>Cancel</button>
+                          <button className="done" onClick={AddItmes} >Done</button>
+                         </div>
+                    </div>
+                  </Modal>
+              </div>
+              <Link to='/'><button className="cancelhome">Cancel</button></Link>
+              <button type="submit" className="formsubmit">Save</button>
+              </div>
             </form>
-          </Modal>
           </div>
-          </div>
-          <Link to='/'><button className="cancelhome">Cancel</button></Link>
-          <button type="submit" className="formsubmit">Save</button>
-          </form>
-        </div>
     )
 }
 
--- frontend/src/components/home .js
+++ frontend/src/components/home .js
@@ -2,8 +2,21 @@ import React from "react";
 import {Link} from "react-router-dom"
 import "./home.css"
 import { AiOutlineSearch } from "react-icons/ai";
+import axios from "axios"
+import {useEffect, useState} from 'react';
 
 function Home(){
+
+  const [postData, setPostData] = useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost:3005/image").then((res)=>{
+            let data = res.data.reverse();
+            setPostData(data);
+            console.log(data);
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }, [])
     return (
         <div className="home-body">
           <div className="home">
@@ -19,7 +32,23 @@ function Home(){
               <Link to="/addsongs"><div className="addSongs">+  Add Songs</div></Link>
             </div>
           </div>
+          <div className='cart'>
+            {postData.map((data, index) => {
+                return (
+                  <div>
+                    <div key={index} className='cart_child' type="none">
+                     <h2>{data.song_name}</h2>
+                     <h1>{data.dor}</h1>
+                     <img src={data.image} alt={"cover"} className="cover-track"></img>
+                     <h3>{data.artist_name}</h3>
+                    </div>
+                  </div>
+                    
+                )
+            })}
         </div>
+        </div>
+        
       );
 }
 
